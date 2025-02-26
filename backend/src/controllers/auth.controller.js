0829f0cbcb3788c349fac7143f3dc7708222b01c
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2025-02-26 15:44:43 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -276,6 +276,7 @@ export const verifyOTPAndLogin = async (req, res) => {
       httpOnly: false,
       secure: true,
       sameSite: "None",
+      domain: ".onrender.com", // Use parent domain
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
 
