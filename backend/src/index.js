1b1581d17c84acf106e184497474a0d89d6d683c
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2025-02-26 11:51:52 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/groups", groupRoutes);
-if (process.env.CLIENT_URL === "production") {
+if (process.env.CLIENT_URL) {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
