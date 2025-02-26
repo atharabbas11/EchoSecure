b31cefd2ed3f2431473e04ceb37a604cc354dac9
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2025-02-26 11:39:17 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -71,30 +71,30 @@ export const verifyOTPAndLogin = async (req, res) => {
     // Set cookies
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.CLIENT_URL === "production",
       sameSite: "None",
       maxAge: 60 * 60 * 1000, // 1 hr minutes
     });
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.CLIENT_URL === "production",
       sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     res.cookie("sessionId", sessionId, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.CLIENT_URL === "production",
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
     res.cookie("csrfToken", csrfToken, {
       httpOnly: true, // CSRF token is HttpOnly
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.CLIENT_URL === "production",
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
     res.cookie("csrfTokenHeader", csrfToken, {
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.CLIENT_URL === "production",
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     });
@@ -151,7 +151,7 @@ export const refreshToken = async (req, res) => {
     // Set the new access token in cookies
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.CLIENT_URL === "production",
       sameSite: "None",
       maxAge: 15 * 60 * 1000, // 15 minutes
     });
